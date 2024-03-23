342e820091221b695562bd21e0afb1c26facb52f
kyawzayarlynn <zipzep327@gmail.com>
2024-03-23 18:11:53 +0630

    sign up form/login form design

---
+++ src/components/Signup.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>Sign up</h2>
+    <form @submit.prevent="signUp">
+      <input type="text" placeholder="display name" v-model="displayName">
+      <input type="email" placeholder="email" v-model="email">
+      <input type="password" placeholder="password" v-model="password">
+      <button>Sign up</button>
+    </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    let displayName = ref("");
+    let email = ref("");
+    let password = ref("");
+    let signUp = () => {
+      
+    }
+
+    return { displayName, email, password , signUp };
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore,Timestamp } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2ZFQLF_ZCSXUF5jAryhsVLCgLC8ugUOU",
+  authDomain: "vue-blog-system-8aa7a.firebaseapp.com",
+  projectId: "vue-blog-system-8aa7a",
+  storageBucket: "vue-blog-system-8aa7a.appspot.com",
+  messagingSenderId: "149863406280",
+  appId: "1:149863406280:web:364fdd1bf41b04fc78a3fd"
+};
+
+const app = initializeApp(firebaseConfig);
+let db = getFirestore(app);
+let timestamp = Timestamp;
+
+export { db, timestamp };
