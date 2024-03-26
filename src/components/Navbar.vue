2ae83deeb659ca2a02bc246a836a958d8c096d93
kyawzayarlynn <zipzep327@gmail.com>
2024-03-26 14:08:13 +0630

    code refactoring for getting current user

---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav v-if="user">
+    <div>
+      <p>Hi {{ user.displayName }}</p>
+      <p class="email">logged in as {{user.email}}</p>
+    </div>
+    <button @click="logout">Logout</button>
+  </nav>
+</template>
+
+<script>
+import { ref } from 'vue';
+import getUser from "../../src/composables/getUser"
+export default {
+  setup()
+  {
+    let error = ref(null);
+    let { user } = getUser();
+    let logout = async() => {
+      try {
+        await signOut(auth);
+      } catch (err) {
+        error.value = err.message;
+      }
+      
+    }
+    return {logout,user}
+  }
+}
+</script>
+
+<style>
+nav {
+  padding: 20px;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+nav p {
+  margin: 2px auto;
+  font-size: 16px;
+  color: #444;
+}
+nav p.email {
+  font-size: 14px;
+  color: #999;
+}
+</style>
---
+++ src/composables/getUser.js
@@ -0,0 +1,14 @@
+import { auth } from "@/firebase/config";
+import { onAuthStateChanged } from "firebase/auth";
+import { ref } from "vue";
+
+let user = ref(auth.currentUser);
+onAuthStateChanged(auth, (_user) => {
+  user.value = _user
+})
+
+let getUser = () => {
+  return { user };
+}
+
+export default getUser;
