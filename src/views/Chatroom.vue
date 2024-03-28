f36587b6030c464cbe6da461bb3d87e3680a8ef2
kyawzayarlynn <zipzep327@gmail.com>
2024-03-28 12:21:43 +0630

    creating chat window and design

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
     <Navbar></Navbar>
+    <ChatWindow></ChatWindow>
     <NewChatForm></NewChatForm>
   </div>
 </template>
 
 <script>
+import ChatWindow from '../components/ChatWindow'
 import NewChatForm from '../components/NewChatForm'
 import { watch } from 'vue'
 import Navbar from '../components/Navbar'
@@ -14,6 +16,7 @@ import { useRouter } from 'vue-router'
 
 export default {
   components: {
+    ChatWindow,
     NewChatForm, Navbar },
   setup() {
     let router = useRouter();
